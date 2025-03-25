a72745686ba6cafd5f2d3c71ae6d437d44444fdc
Sandro <alexanderkapanadze52@gmail.com>
2025-03-25 22:32:37 +0400

    added remove and clear functionality for canvas

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,16 @@ import {
   handleCanvaseMouseMove,
   handleCanvasMouseDown,
   handleCanvasMouseUp,
+  handleCanvasObjectModified,
   handleResize,
   initializeFabric,
   renderCanvas,
 } from "@/lib/canvas"
 import { ActiveElement } from "@/types/type"
 import { useMutation, useStorage } from "@liveblocks/react"
+import { LiveMap } from "@liveblocks/core"
+import { defaultNavElement } from "@/constants"
+import { handleDelete } from "@/lib/key-events"
 export default function Page() {
   const canvasRef = useRef<HTMLCanvasElement>(null)
   const fabricRef = useRef<fabric.Canvas | null>(null)
@@ -41,9 +45,38 @@ export default function Page() {
     canvasObjects?.set(objectId, shapeData)
   }, [])
 
+  const deleteAllShapes = useMutation(({ storage }) => {
+    const canvasObjects = storage.get("canvasObjects")
+    if (!canvasObjects || canvasObjects.size === 0) return true
+
+    for (const [key, _] of canvasObjects.entries()) {
+      canvasObjects.delete(key)
+    }
+
+    return canvasObjects.size === 0
+  }, [])
+
+  const deleteShapeFromStorage = useMutation(({ storage }, objectId) => {
+    const canvasObjects = storage.get("canvasObjects")
+    if (!canvasObjects || canvasObjects.size === 0) return
+
+    canvasObjects.delete(objectId)
+  }, [])
+
   const handleActiveElement = (elem: ActiveElement) => {
     setActiveElement(elem)
-
+    switch (elem?.value) {
+      case "reset":
+        deleteAllShapes()
+        fabricRef.current?.clear()
+        setActiveElement(defaultNavElement)
+        break
+      case "delete":
+        handleDelete(fabricRef.current as any, deleteShapeFromStorage)
+        setActiveElement(defaultNavElement)
+      default:
+        break
+    }
     selectedShapeRef.current = elem?.value as string
   }
 
@@ -79,14 +112,25 @@ export default function Page() {
         activeObjectRef,
       })
     })
+    canvas.on("object:modified", (options) => {
+      handleCanvasObjectModified({
+        options,
+        syncShapeInStorage,
+      })
+    })
 
     window.addEventListener("resize", () => {
       handleResize({ canvas })
     })
+
+    return () => {
+      canvas.dispose()
+    }
   }, [])
 
   useEffect(() => {
-    renderCanvas({ fabricRef, canvasObjects, activeObjectRef })
+    if (fabricRef && canvasObjects && activeObjectRef)
+      renderCanvas({ fabricRef, canvasObjects, activeObjectRef })
   }, [canvasObjects])
 
   return (
